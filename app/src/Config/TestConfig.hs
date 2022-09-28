3870357a9c6b32a8c54583dac34f26760c54e3ee
ChromaChroma <jona.beer@gmail.com>
2022-09-28 16:42:04 +0200

    Update gitignore Add inital gloss render

--- app/src/Config/TestConfig.hs
+++ app/src/Config/TestConfig.hs
@@ -1,6 +1,77 @@
 module Config.TestConfig where
-  
-import Graphics.Gloss
 
-window :: Display
-window = InWindow "Nice Window" (400, 400) (10, 10)
+import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Interface.IO.Game
+
+
+type Model = (Float, Float)
+
+screen :: Display
+screen = InWindow "Nice Window" windowSize windowOffsetPosition
+-- screen = FullScreen
+
+windowSize :: (Int, Int)
+windowSize = (1000, 1000)
+
+windowOffsetPosition :: (Int, Int)
+windowOffsetPosition = (0, 0)
+
+-- backgroundColor :: Color
+-- backgroundColor = black
+
+fps :: Int
+fps = 60
+
+-- main :: IO ()
+-- main = simulate
+--   screen
+--   white
+--   simulationRate
+--   initialModel
+--   drawingFunc
+--   updateFunc
+--   where
+--     simulationRate :: Int
+--     simulationRate = 60
+
+--     initialModel :: Model
+--     initialModel = (0,0)
+
+--     drawingFunc :: Model -> Picture
+--     drawingFunc (theta, dtheta) = Line [(0, 0), (50 * cos theta, 50 * sin theta)]
+
+--     updateFunc :: ViewPort -> Float -> Model -> Model
+--     updateFunc _ dt (theta, dtheta) = (theta + dt * dtheta, dtheta - dt * (cos theta))
+
+startRender :: IO ()
+startRender = play
+  screen
+  white
+  fps
+  initialModel
+  drawingFunc
+  inputHandler
+  updateFunc
+  where
+    initialModel :: Model
+    initialModel = (0,0)
+
+    drawingFunc :: Model -> Picture
+    drawingFunc (x, y) = translate x y (Circle 20)
+
+    inputHandler :: Event -> Model -> Model
+    inputHandler (EventKey (SpecialKey KeyUp) Down _ _) (x, y) = (x, y + 10)
+    inputHandler (EventKey (SpecialKey KeyDown) Down _ _) (x, y) = (x, y - 10)
+    inputHandler (EventKey (SpecialKey KeyRight) Down _ _) (x, y) = (x + 10, y)
+    inputHandler (EventKey (SpecialKey KeyLeft) Down _ _) (x, y) = (x - 10, y)
+    inputHandler _ w = w
+
+    updateFunc :: Float -> Model -> Model
+    updateFunc _ (x, y) = (towardCenter x, towardCenter y)
+      where
+        towardCenter :: Float -> Float
+        towardCenter c
+          | abs c < 0.25 = 0
+          | c > 0 = c - 0.25
+          | otherwise = c + 0.25
--- app/src/View/TestView.hs
+++ app/src/View/TestView.hs
@@ -7,16 +7,10 @@ import Model.TestModel
 background :: Color
 background = white
 
--- drawing :: Picture
--- drawing = circle 80
-
--- showCircle :: IO ()
--- showCircle = do
---   display window background drawing
-
 getName :: TestModel -> Picture
 getName (MkTestModel name) = text name
 
 showText :: TestModel -> IO ()
 showText m = do
-  display window background (getName m)
+  startRender
+  -- display screen background (getName m)
