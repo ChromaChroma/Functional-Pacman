d071fcdbfeff13b30a564ee908d7c05196c2faed
ChromaChroma <jona.beer@gmail.com>
2022-10-04 21:56:21 +0200

    Reformat Gui order Add input handling

--- app/src/View/Gui.hs
+++ app/src/View/Gui.hs
@@ -4,18 +4,18 @@ import Model.Game
 import Model.Characters
 import Model.Items
 import Model.Score
-import Model.Movement
+import Model.Movement as M
 import Model.Level
 import Controller.Engine
 
 
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
-import Graphics.Gloss.Interface.IO.Game
-import Data.Data (ConstrRep(FloatConstr))
+import Graphics.Gloss.Interface.IO.Game as IO
 import Data.Maybe
 import Data.List
 import Data.List.Index
+import Graphics.Gloss.Interface.IO.Game (SpecialKey(KeyEsc), Key (SpecialKey))
 
 screen :: Display
 screen = InWindow "Pac Man" windowSize windowOffsetPosition
@@ -28,7 +28,7 @@ windowOffsetPosition :: (Int, Int)
 windowOffsetPosition = (0, 0)
 
 fps :: Int
-fps = 10
+fps = 60
 
 tileSize :: Float
 tileSize = 32
@@ -37,50 +37,44 @@ startRender :: IO ()
 startRender =
   play
     screen
-    white
+    black
     fps
     initialModel
     drawingFunc
     inputHandler
     updateFunc
+
+-- | Initial state of the game at startup
+initialModel :: GameState
+initialModel = defaultGame
+
+-- | Render game state, aligned from bottom left corner
+drawingFunc :: GameState -> Picture
+drawingFunc gs = fromBottomLeft $ pictures [
+  renderGame gs,
+  renderOverlay gs
+  ]
   where
-    initialModel :: GameState
-    initialModel = defaultGame
+    (lx, ly) = layoutSize $ layout $ level gs
+    (x, y ) = windowSize
+    x' = - fromIntegral(x `div` 2) + tileSize / 2
+    y' = - fromIntegral (y `div` 2) + tileSize / 2
+    fromBottomLeft = translate x' y'
 
-    -- | Render game state, aligned from bottom left corner
-    drawingFunc :: GameState -> Picture
-    drawingFunc gs = fromBottomLeft $ pictures [
-        -- smallText . show . status $ gs,
-        -- smallText . show . elapsedTime $ gs,
-        -- smallText . show . score $ gs,
-        -- smallText . show . lives $ gs,
-        -- smallText . show . level $ gs,
-        smallText . show . pPosition . player $ gs,
+    renderGame gs = pictures[
+      renderLevel . level $ gs,
+      renderPlayer gs
+      -- Render Movable?? ghost and player get rendered same way.
+      -- renderGhosts . ghosts $ gs
+      -- renderItems . items . level $ gs
+      -- PointItems
+      ]
 
-        renderLevel defaultLevel,
-        -- PointItems
-
-        renderPlayer gs
-        -- Render Movable?? ghost and player get rendered same way.
-        ]
-        where
-          fromBottomLeft = translate x' y'
-          
-          (x, y) = windowSize
-          x' = - fromIntegral(x `div` 2) + tileSize / 2
-          y' = - fromIntegral (y `div` 2) + tileSize / 2
-
-          smallText = scale 0.1 0.1 . text 
-
-    inputHandler :: Event -> GameState -> GameState
-    -- inputHandler (EventKey (SpecialKey KeyUp) Down _ _) (x, y) = (x, y + 10)
-    -- inputHandler (EventKey (SpecialKey KeyDown) Down _ _) (x, y) = (x, y - 10)
-    -- inputHandler (EventKey (SpecialKey KeyRight) Down _ _) (x, y) = (x + 10, y)
-    -- inputHandler (EventKey (SpecialKey KeyLeft) Down _ _) (x, y) = (x - 10, y)
-    inputHandler _ w = w
-
-    updateFunc :: Float -> GameState -> GameState
-    updateFunc _ gs = step 50 gs
+    renderOverlay gs = translate 0 (fromIntegral ly *  tileSize) $ pictures[
+      -- renderLives . pLives . player $ gs
+      -- renderScore . score $ gs
+      renderDebug gs
+      ]
 
 -- | Returns Pictures, consisting of all tile Pictures
 renderLevel :: Level -> Picture
@@ -109,9 +103,6 @@ renderPlayer gs =
         x' =  fromIntegral x * tileSize
         y' =  (fromIntegral ly * tileSize) - fromIntegral ((y+1) * round tileSize )
 
-
-
-
 -- | Returns Pictures (Picture consisting of multiple pictures
 renderGhosts :: [Ghost] -> Picture
 renderGhosts = undefined
@@ -125,3 +116,36 @@ renderLives = undefined
 
 renderScore :: Score -> Picture
 renderScore = undefined
+
+renderDebug :: GameState -> Picture
+renderDebug gs = pictures $ stack 0 100 [
+  -- translate 0 200 . smallText . show . score $ gs,
+  smallText . status $ gs,
+  smallText . elapsedTime $ gs,
+  smallText . unlives . pLives . player $ gs,
+  smallText . direction $ gs,
+  smallText . bufDirection $ gs,
+  smallText . pPosition . player $ gs
+  ]
+  where
+    smallText :: Show a => a -> Picture
+    smallText = color white . scale 0.1 0.1 . text . show
+
+    stack :: Float -> Float -> [Picture] -> [Picture]
+    stack _ _ []  = []
+    stack x y (l:ls) = translate x y l : stack x (y + 25) ls
+
+-- | Input handling 
+inputHandler :: Event -> GameState -> GameState
+inputHandler (EventKey (SpecialKey KeyUp) IO.Down _ _) gs = movePlayer M.Up gs
+inputHandler (EventKey (SpecialKey KeyDown) IO.Down _ _) gs = movePlayer M.Down gs
+inputHandler (EventKey (SpecialKey KeyRight) IO.Down _ _) gs = movePlayer M.Right gs
+inputHandler (EventKey (SpecialKey KeyLeft) IO.Down _ _) gs = movePlayer M.Left gs
+inputHandler (EventKey (Char 'p') IO.Down _ _) gs = pause gs
+inputHandler (EventKey (Char 'r') IO.Down _ _) gs = resume gs
+inputHandler (EventKey (Char 'q') IO.Down _ _) gs = quit gs
+inputHandler _ w = w
+
+-- | Update function ran each iteration
+updateFunc :: Float -> GameState -> GameState
+updateFunc s gs = let ms = round (s * 1000) in step ms gs
