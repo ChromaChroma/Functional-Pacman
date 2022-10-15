198a6175e824812402fb16a18636ba438d441094
ChromaChroma <jona.beer@gmail.com>
2022-10-16 01:48:40 +0200

    Add ability for player to eat ghosts

--- app/src/Model/Game.hs
+++ app/src/Model/Game.hs
@@ -9,10 +9,10 @@ module Model.Game
   )
 where
 
-import Model.Ghosts (Ghost, blinky, clyde, inky, pinky)
+import Model.Ghosts (Ghost (lifeState, mode), blinky, clyde, inky, pinky, GhostState (Frightened), LifeState (Alive, Eaten))
 import qualified Model.Items as I
 import Model.Level (Level (items, playerSpawn), defaultLevel)
-import Model.Movement (Collidable (collides), Direction (..), Positioned (setPosition))
+import Model.Movement (Collidable (collides), Direction (..), Positioned (setPosition), Movable (getSpeed))
 import Model.Player (Player (lives), defaultPlayer, isAlive, position, rmLife)
 import Model.Score (Points)
 import Prelude hiding (Left, Right)
@@ -61,7 +61,7 @@ defaultGame = loadGame lvl ghosts pl
 checkGameOver :: GameState -> GameState
 checkGameOver gs
   | isAlive' = gs
-  | otherwise = gs {status = GameOver} 
+  | otherwise = gs {status = GameOver}
   where
     isAlive' = isAlive . lives . player $ gs
 
@@ -74,11 +74,24 @@ checkCollisions :: GameState -> GameState
 checkCollisions = checkGhostCollisions . checkItemCollisions
 
 checkGhostCollisions :: GameState -> GameState
-checkGhostCollisions gs =
-  if any (player gs `collides`) $ ghosts gs
-    then respawnPlayer . reduceLife $ gs
-    else gs
+checkGhostCollisions gs = handleGhostCollisions gs (filter (\g -> player gs `collides` g && lifeState g == Alive) $ ghosts gs)
+  -- let collidingGhosts = filter (player gs `collides`) $ ghosts gs
+  --  in if not (null collidingGhosts)
+  --       then respawnPlayer . reduceLife $ gs
+  --       else gs
+  -- where
+  --   reduceLife gs = gs {player = (player gs) {lives = rmLife . lives . player $ gs}}
+  --   respawnPlayer gs
+  --     | isAlive . lives $ player gs = gs {player = (player gs) {position = playerSpawn . level $ gs}}
+  --     | otherwise = gs {status = GameOver}
+
+handleGhostCollisions :: GameState -> [Ghost] -> GameState
+handleGhostCollisions gs [] = gs
+handleGhostCollisions gs (g : _) = if (lifeState g == Alive) && (mode g == Frightened)
+  then gs {ghosts = map (\x -> if x == g then x {lifeState = Eaten} else x) (ghosts gs)}
+  else respawnPlayer . reduceLife $ gs
   where
+    eatGhost g = g {lifeState = Eaten}
     reduceLife gs = gs {player = (player gs) {lives = rmLife . lives . player $ gs}}
     respawnPlayer gs
       | isAlive . lives $ player gs = gs {player = (player gs) {position = playerSpawn . level $ gs}}
@@ -90,5 +103,5 @@ checkItemCollisions gs = foldr (\item -> removeItem item . addItemScore item . h
     removeItem item gs = gs {level = (level gs) {items = filter (/= item) (items . level $ gs)}}
     addItemScore item gs = gs {points = points gs + I.points item}
     handleItemType item gs = case item of
-      I.PowerPellet {} -> gs -- todo: Check if item is power pellet, if so, set ghost state to weak
+      I.PowerPellet _ _  -> gs {ghosts = map (\x -> x {mode = Frightened}) (ghosts gs)}
       _ -> gs
