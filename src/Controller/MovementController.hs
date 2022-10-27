1412e759aa36fba9cc104645d523df25b3c19fed
sembodi <aapje11g@gmail.com>
2022-10-27 16:33:19 +0200

    in GhostAI zelf functie gemaakt en canMakeMovetoDir type veranderd van Player naar Movable (zodat ghost ook die kan gebruiken)

--- src/Controller/MovementController.hs
+++ src/Controller/MovementController.hs
@@ -43,7 +43,7 @@ makeDirectionMove gs dir
     pl = player gs
     lvl = level gs
 
-canMakeMoveToDir :: Player -> Direction -> Level -> Bool
+canMakeMoveToDir :: Movable -> Direction -> Level -> Bool
 canMakeMoveToDir player dir lvl
   | isValid = case dir of
     Up -> canMovePerpendicular x
