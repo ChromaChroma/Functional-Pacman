f7bb577748f5e57fde5357f2e9317a7db2dd8634
ChromaChroma <jona.beer@gmail.com>
2022-11-09 14:35:04 +0100

    Add intPosition To check if on spawn or target tile

--- src/Model/GhostAI.hs
+++ src/Model/GhostAI.hs
@@ -24,8 +24,8 @@ makeGhostMoveEv :: GameState -> Ghost -> Ghost
 makeGhostMoveEv gs ghst
   | not $ isEaten ghst = makeGhostMove gs ghst
   | not $ goesBack ghst = (makeGhostMove gs ghst) {G.position = gTilePos, G.speed = 1 / 2, goesBack = True} --start to bring ghost back to spawn
-  | gTile == spawnPoint ghst = (makeGhostMove gs ghst) {G.position = gTilePos, eatenState = NotEaten, G.speed = 0.125, G.direction = Up, opDirection = Down, nextDirection = nextDir, goesBack = False, wellPositionedTarget = False}
-  | gTile /= targetpoint || wellPositionedTarget ghst = makeGhostMove gs ghst --gaat op target tile vlakbij spawn af.
+  | gTile == (intPosition $ spawnPoint ghst) = (makeGhostMove gs ghst) {G.position = gTilePos, eatenState = NotEaten, G.speed = 0.125, G.direction = Up, opDirection = Down, nextDirection = nextDir, goesBack = False, wellPositionedTarget = False}
+  | gTile /= (intPosition $ targetpoint) || wellPositionedTarget ghst = makeGhostMove gs ghst --gaat op target tile vlakbij spawn af.
   | otherwise = (makeGhostMove gs ghst) {G.position = gTilePos, G.direction = Down, opDirection = Up, wellPositionedTarget = True}
   where
     nextDir = case name ghst of
@@ -46,7 +46,7 @@ makeGhostMoveEv gs ghst
       Clyde -> (14, 19)
 
     gTilePos = ghostTilePosition ghst
-    gTile = getPosition ghst
+    gTile = intPosition $ getPosition ghst
 
 makeGhostMove :: GameState -> Ghost -> Ghost
 makeGhostMove gs ghst
